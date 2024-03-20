ddffecd59c8def9c8ad8bba9ffa1d121766add6c
Shivani Patil <patilshivani420@gmail.com>
2024-03-20 17:43:59 +0530

    Setup DbContext Class

---
+++ DbOperationWithEFCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationWithEFCoreApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- DbOperationWithEFCoreApp/Program.cs
+++ DbOperationWithEFCoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationWithEFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationWithEFCoreApp
 {
     public class Program
@@ -8,6 +11,9 @@ namespace DbOperationWithEFCoreApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
